136f493d25ecda74a036eb399c9d5eb5c74d4150
MarcoGranata9 <granatam9900@gmail.com>
2023-12-14 18:03:30 +0100

    add: off-canvas

--- script.js
+++ script.js
@@ -5,10 +5,22 @@ createApp({
         return {
             apiUrl: "server.php",
             diskArray: [],
+            singleDisk: [],
+            show: false,
         }
     },
     methods: {
-
+        diskDetails(index) {
+            this.show = true;
+            axios.get(this.apiUrl).then((resp) => {
+                console.log(resp.data[index]);
+                this.singleDisk = resp.data[index];
+            })
+        },
+        closeCanvas() {
+            this.show = false;
+            this.singleDisk = [];
+        }
     },
     created() {
         axios.get(this.apiUrl).then((resp) => {
